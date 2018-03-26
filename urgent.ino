7c9ae582ade7f7c44eef9ce546f1ef5be1bf21f3
sherrieguan <35797658+sherrieguan@users.noreply.github.com>
2018-03-25 23:27:33 -0700

    Create urgent.ino

---
+++ urgent.ino
@@ -0,0 +1,28 @@
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorPin = A0;    // select the input pin for the potentiometer
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+}
+void loop() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 50) {
+    // sets the value (range from 0 to 255):
+    analogWrite(5, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 50) {
+    // sets the value (range from 0 to 255):
+    analogWrite(5, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+}
